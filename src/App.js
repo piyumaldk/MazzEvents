076e339902bc6c21336f1828fca859c7ff219989
Piyumal DK <38649090+piyumaldk@users.noreply.github.com>
2019-08-28 18:52:28 +0530

    Update home 1.1

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <button>Log in</button>
+      <p ClassName="Title">MazzEvents</p>   
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          MazzEvents
-        </p>        
-        <button>Log in</button>
+        <img src={logo} className="App-logo" alt="logo" />     
       </header>
+      
     </div>
   );
 }
