c0eb48da1c812872e0ee321cf84deb0194ce5a7b
Piyumal DK <38649090+piyumaldk@users.noreply.github.com>
2019-08-28 19:32:03 +0530

    Update home v1.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import logo from './logo.jpg';
 import './App.css';
+import {Button} from 'react-bootstrap';
+import {Nav} from 'react-bootstrap';
 
 function App() {
   return (
+
+    
+
+
     <div className="App">
-      <button>Log in</button>
+
+<Nav justify variant="tabs" defaultActiveKey="/home">
+  <Nav.Item>
+    <Nav.Link href="/home">HOME</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-1">EVENTS</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-2">ABOUT US</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="link-3">LOG IN</Nav.Link>
+  </Nav.Item>
+</Nav>
+
+
+
+      
       <p ClassName="Title">MazzEvents</p>   
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />     
