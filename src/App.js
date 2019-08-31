1b450613c04ebe4efa3ac13cfe167212064405d1
piyumaldk <piyumaldk@gmail.com>
2019-08-31 17:25:10 +0530

    Add Navbar and Login

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
+
+
 
 import Home from "./Components/Home.component";
 import LogIn from "./Components/LogIn.component";
-
+import AboutUs from "./Components/AboutUs.component";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="container">
-          <h2>MazzEvents - nipuni</h2>
+        <div>
+          <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="/">MazzEvents</Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/login">Log in</Nav.Link>
+              <Nav.Link href="/aboutus">About us</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-info">Search Events</Button>
+            </Form>
+          </Navbar>
+
           <Route path="/" exact component={Home} />
           <Route path="/login" component={LogIn} />
+          <Route path="/aboutus" component={AboutUs} />
         </div>
       </Router>
     );
---
+++ src/Components/AboutUs.component.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+export default class AboutUs extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to About US!!</p>
+            </div>
+        )
+    }
+}
