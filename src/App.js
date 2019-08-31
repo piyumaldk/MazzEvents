a6285a328b864d539985756c5786d1f6979f26af
piyumaldk <piyumaldk@gmail.com>
2019-08-31 12:09:41 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -1,39 +1,27 @@
-import React from 'react';
-import logo from './logo.jpg';
+import React, {Components} from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import './App.css';
 import {Nav} from 'react-bootstrap';
 
+
 function App() {
   return (
-
-    
-
-
     <div className="App">
-
-<Nav justify variant="tabs" defaultActiveKey="/home">
-  <Nav.Item>
-    <Nav.Link href="/home">HOME</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-1">EVENTS</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-2">ABOUT US</Nav.Link>
-  </Nav.Item>
-  <Nav.Item>
-    <Nav.Link eventKey="link-3">LOG IN</Nav.Link>
-  </Nav.Item>
-</Nav>
-
-
-
-      
+      <Nav justify variant="tabs" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">HOME</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">EVENTS</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">ABOUT US</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-3">LOG IN</Nav.Link>
+        </Nav.Item>
+      </Nav>
       <p ClassName="Title">MazzEvents - Colombo</p>   
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />     
-      </header>
-      
     </div>
   );
 }
