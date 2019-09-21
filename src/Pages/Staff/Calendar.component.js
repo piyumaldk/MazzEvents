a88ee54c81879d2e2e13dc695649cb15b40c0e86
piyumaldk <piyumaldk@gmail.com>
2019-09-21 16:06:20 +0530

    Add MD Boostrap React

--- src/Pages/Staff/Calendar.component.js
+++ src/Pages/Staff/Calendar.component.js
@@ -1,10 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { MDBListGroup, MDBListGroupItem, MDBContainer } from "mdbreact";
 
-import LeftStaff from "../../Components/LeftStaff.component";
-export default class StaffDash extends Component {
-    render() {
-        return (
-            <LeftStaff/>
-        )
-    }
-}
+const StaffCalendar = () => {
+  return (
+    <MDBContainer>
+      <MDBListGroup style={{ width: "22rem" }}>
+        <MDBListGroupItem>Cras justo odio</MDBListGroupItem>
+        <MDBListGroupItem>Dapibus ac facilisis in</MDBListGroupItem>
+        <MDBListGroupItem>Morbi leo risus</MDBListGroupItem>
+        <MDBListGroupItem>Porta ac consectetur ac</MDBListGroupItem>
+        <MDBListGroupItem>Vestibulum at eros</MDBListGroupItem>
+      </MDBListGroup>
+    </MDBContainer>
+  );
+};
+
+export default StaffCalendar;
