6487e4330378ecec3a36ac570efdeba5f773638f
piyumaldk <piyumaldk@gmail.com>
2019-09-22 13:13:32 +0530

    Customer Navigation Bar Updated

--- src/App.js
+++ src/App.js
@@ -25,45 +25,6 @@ class App extends Component {
           <Route path="/staff/calendar" component={StaffCalendar} />
           <Route path="/customer/dash" component={LeftCustomer} />
         </div>
-        <Route render={({ location, history }) => (
-        <React.Fragment>
-            <SideNav
-                onSelect={(selected) => {
-                    const to = '/' + selected;
-                    if (location.pathname !== to) {
-                        history.push(to);
-                    }
-                }}
-            >
-                <SideNav.Toggle />
-                <SideNav.Nav defaultSelected="home">
-                    <NavItem eventKey="home">
-                        <NavIcon>
-                            <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-                        </NavIcon>
-                        <NavText>
-                            Home
-                        </NavText>
-                    </NavItem>
-                    <NavItem eventKey="devices">
-                        <NavIcon>
-                            <i className="fa fa-fw fa-device" style={{ fontSize: '1.75em' }} />
-                        </NavIcon>
-                        <NavText>
-                            Devices
-                        </NavText>
-                    </NavItem>
-                </SideNav.Nav>
-            </SideNav>
-            <main>
-                
-                <Route path="/home" component={props => <Home />} />
-                {/* <Route path="/devices" component={props => <Devices />} />
-                <Route path="/" exact component={props => <RootComponent />} /> */}
-            </main>
-        </React.Fragment>
-    )}
-    />
       </Router>
     );
   }
--- src/Components/LeftCustomer.component.js
+++ src/Components/LeftCustomer.component.js
@@ -2,49 +2,49 @@ import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/rea
 import '@trendmicro/react-sidenav/dist/react-sidenav.css';
 import React, { Component } from 'react';
 import '../App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from "../Pages/Home.component";
 
 export default class LeftStaff extends Component {
     render() {
         return (
-            <div class="sidenav">
-                <SideNav
-    onSelect={(selected) => {
-        // Add your code here
-    }}
->
-    <SideNav.Toggle />
-    <SideNav.Nav defaultSelected="home">
-        <NavItem eventKey="home">
-            <NavIcon>
-                <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-                Home
-            </NavText>
-        </NavItem>
-        <NavItem eventKey="charts">
-            <NavIcon>
-                <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
-            </NavIcon>
-            <NavText>
-                Charts
-            </NavText>
-            <NavItem eventKey="charts/linechart">
-                <NavText>
-                    Line Chart
-                </NavText>
-            </NavItem>
-            <NavItem eventKey="charts/barchart">
-                <NavText>
-                    Bar Chart
-                </NavText>
-            </NavItem>
-        </NavItem>
-    </SideNav.Nav>
-</SideNav>
-            </div>
-
-            
+            <div>
+                <Router>
+                    <Route render={({location,history})=>(
+                        <React.Fragment>
+                            <SideNav onSelect={(selected)=>{
+                                    const to = '/' + selected;
+                                    if (location.pathname !== to) {
+                                        history.push(to);
+                                    }
+                            }}>
+                                <SideNav.Toggle/>
+                                <SideNav.Nav defaultSelected="home">
+                                    <NavItem eventKey="home">
+                                        <NavIcon><i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} /></NavIcon>
+                                        <NavText>Customer Dashboard</NavText>
+                                    </NavItem>
+                                    <NavItem eventKey="charts">
+                                        <NavIcon><i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} /></NavIcon>
+                                        <NavText>Charts</NavText>
+                                        <NavItem eventKey="charts/linechart">
+                                            <NavText>Line Chart</NavText>
+                                        </NavItem>
+                                        <NavItem eventKey="charts/barchart">
+                                            <NavText>Bar Chart</NavText>
+                                        </NavItem>
+                                    </NavItem>
+                                </SideNav.Nav>
+                            </SideNav>
+                            <main>
+                                <Route path="/home" component={props => <Home />} />
+                                {/* <Route path="/devices" component={props => <Devices />} />
+                                <Route path="/" exact component={props => <RootComponent />} /> */}
+                            </main>
+                        </React.Fragment>
+                    )}/>
+                </Router>    
+            </div>   
         )
     }
 }
