a8a4aa9c6d54719fd6ba596da48180c1e0ad57e7
nipuni-d <nipuniwarakagoda97@gmail.com>
2019-09-25 17:48:27 +0530

    Update service provider

--- src/Pages/ServiceProvider/Account.component.js
+++ src/Pages/ServiceProvider/Account.component.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import LeftSeriveProvider from "../../Components/LeftServiceProvider.component";
+import {Table} from 'react-bootstrap';
+import {Button, ButtonGroup} from 'react-bootstrap';
 
 export default class ServiceProviderAccount extends Component {
     render() {
         return (
             <div>
                 <LeftSeriveProvider/>
-                <div class="right">
-                    This is Serivce Proivder - Account
-                </div>
+                <div class="right">Service Provider Account
+
+                <Table striped bordered hover size="sm">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>Name</th>
+      <th>Service Type</th>
+      <th>Address</th>
+      <th>Email Address</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </tbody>
+</Table>
+ <ButtonGroup aria-label="Basic example">
+  <Button variant="secondary">Edit Account</Button>
+  <Button variant="secondary">Delete Account</Button>
+  <Button variant="secondary">Edit Service</Button>
+  <Button variant="secondary">Delete Service</Button>
+</ButtonGroup>
+            </div>
                 
             </div>   
         )
