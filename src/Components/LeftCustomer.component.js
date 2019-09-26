a226401b70170ccfd8ad865965cb29504bef367e
kabsgithub <36620228+kabsgithub@users.noreply.github.com>
2019-09-26 10:43:19 +0530

    Edit LeftCustomer component

--- src/Components/LeftCustomer.component.js
+++ src/Components/LeftCustomer.component.js
@@ -5,46 +5,29 @@ import '../App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "../Pages/Home.component";
 
+
 export default class LeftStaff extends Component {
     render() {
         return (
-            <div>
-                <Router>
-                    <Route render={({location,history})=>(
-                        <React.Fragment>
-                            <SideNav onSelect={(selected)=>{
-                                    const to = '/' + selected;
-                                    if (location.pathname !== to) {
-                                        history.push(to);
-                                    }
-                            }}>
-                                <SideNav.Toggle/>
-                                <SideNav.Nav defaultSelected="home">
-                                    <NavItem eventKey="home">
-                                        <NavIcon><i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} /></NavIcon>
-                                        <NavText>Customer Dashboard</NavText>
-                                    </NavItem>
-                                    <NavItem eventKey="charts">
-                                        <NavIcon><i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} /></NavIcon>
-                                        <NavText>Charts</NavText>
-                                        <NavItem eventKey="charts/linechart">
-                                            <NavText>Line Chart</NavText>
-                                        </NavItem>
-                                        <NavItem eventKey="charts/barchart">
-                                            <NavText>Bar Chart</NavText>
-                                        </NavItem>
-                                    </NavItem>
-                                </SideNav.Nav>
-                            </SideNav>
-                            <main>
-                                <Route path="/home" component={props => <Home />} />
-                                {/* <Route path="/devices" component={props => <Devices />} />
-                                <Route path="/" exact component={props => <RootComponent />} /> */}
-                            </main>
-                        </React.Fragment>
-                    )}/>
-                </Router>    
-            </div>   
+            <div class="sidenav">
+                <h5>Customer</h5>
+                <p>Components</p>
+                <a href="/serviceprovider/addservices">Add Services +</a>
+                <a href="/customer/photo">Photographers</a>
+                <a href="/customer/dj">Dj</a>
+                <a href="/customer/music">Music</a>
+                <a href="/customer/catering">Catering</a>
+                <a href="/customer/reception">Reception Halls</a>
+                <a href="/customer/hotels">Hotels</a>
+                <a href="/customer/flowers">Flowers</a>
+                <a href="/customer/vehicles">Vehicles</a>
+                <p>More Pages</p>
+                <a href="/customer/chat">Chat</a>
+                <a href="/customer/mail">Mail Box</a>
+                <a href="/customer/eventscl">Events Calendar</a>
+                <a href="/customer/account">My Account</a>
+                <a href="/customer/rating">Rating</a>
+            </div>
         )
     }
 }
