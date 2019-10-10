5cfee8794d89de10205f484a541b076f87b60143
kabsgithub <36620228+kabsgithub@users.noreply.github.com>
2019-10-10 17:46:04 +0530

    Update card component

--- src/Components/Cads/Cards.jsx
+++ src/Components/Cads/Cards.jsx
@@ -1,27 +1,47 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Card from './CardUI';
 import LeftCustomer from "../../Components/LeftCustomer.component";
 
 import img1 from '../../Images/new.jpg'
 
-class Cards extends Component{
-    render(){
-        return(
-            
-            <div className="container-fluid d-flex justify-content-center">
-                <LeftCustomer/>
-                <div className="row">
-                    <div className="col-md-4">
-                    <Card imgsrc={img1} title="Sachin"/>
-                    </div>
-                    <div className="col-md-4">
-                    <Card imgsrc={img1} title="Ravindu"/>
-                    </div>
-                    <div className="col-md-4">
-                    <Card imgsrc={img1} title="Himash"/>
-                    </div>
-                    
+class Cards extends Component {
+    render() {
+        return (
 
+            <div className="container-fluid d-flex justify-content-center">
+                <div className="col-md-2">
+                    <LeftCustomer />
+                </div>
+                <div className="col-md-10">
+                    <div className="row card_ss">
+                        
+                        <div className="col-md-3">
+                            <Card imgsrc={img1} title="Sachin" />
+                        </div>
+                        <div className="col-md-3">
+                            <Card imgsrc={img1} title="Ravindu" />
+                        </div>
+                        <div className="col-md-3">
+                            <Card imgsrc={img1} title="Himash" />
+                        </div>
+                        <div className="col-md-3">
+                            <Card imgsrc={img1} title="Himash" />
+                        </div>
+                    </div>
+                    <div className="row card_ss">
+                        <div className="col-md-3">
+                            <Card imgsrc={img1} title="Sachin" />
+                        </div>
+                        <div className="col-md-3">
+                            <Card imgsrc={img1} title="Ravindu" />
+                        </div>
+                        <div className="col-md-3">
+                            <Card imgsrc={img1} title="Himash" />
+                        </div>
+                        <div className="col-md-3">
+                            <Card imgsrc={img1} title="Himash" />
+                        </div>
+                    </div>
                 </div>
             </div>
         );
