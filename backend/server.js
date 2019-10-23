f728f8ac4884db2c3a102f636deda52d2c85709f
Piyumal DK <38649090+piyumaldk@users.noreply.github.com>
2019-10-23 14:27:39 +0530

    Latest

---
+++ backend/server.js
@@ -0,0 +1,69 @@
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const signupRoutes = express.Router();
+const PORT = 4000;
+let SignUp = require('./signup.model');
+app.use(cors());
+app.use(bodyParser.json());
+mongoose.connect('mongodb://127.0.0.1:27017/mazzevents', { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
+signupRoutes.route('/').get(function(req, res) {
+    SignUp.find(function(err, mazzevents) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(mazzevents);
+        }
+    });
+});
+signupRoutes.route('/:id').get(function(req, res) {
+    let id = req.params.id;
+    SignUp.findById(id, function(err, signup) {
+        res.json(signup);
+    });
+});
+signupRoutes.route('/update/:id').post(function(req, res) {
+    SignUp.findById(req.params.id, function(err, signup) {
+        if (!signup)
+            res.status(404).send("data is not found");
+        else {
+            signup.signup_firstName = req.body.signup_firstName;
+            signup.signup_lastName = req.body.signup_lastName;
+            signup.signup_email = req.body.signup_email;
+            signup.signup_password = req.body.signup_password;
+            signup.signup_aPassword = req.body.signup_aPassword;
+            signup.signup_number = req.body.signup_number;
+            signup.signup_location = req.body.signup_location;
+            signup.signup_completed = req.body.signup_completed;
+            signup.save().then(signup => {
+                res.json('signup updated!');
+            })
+            .catch(err => {
+                res.status(400).send("Update not possible");
+            });
+        }
+           
+    });
+});
+signupRoutes.route('/add').post(function(req, res) {
+    let signup = new SignUp(req.body);
+    signup.save()
+        .then(signup => {
+            res.status(200).json({'signup': 'signup added successfully'});
+        })
+        .catch(err => {
+            res.status(400).send('adding new signup failed');
+        });
+});
+app.use('/mazzevents', signupRoutes);
+app.listen(PORT, function() {
+    console.log("Server is running on Port: " + PORT);
+});
+
--- frontend/src/Pages/SignUp.component.js
+++ frontend/src/Pages/SignUp.component.js
@@ -3,37 +3,137 @@ import Upper from "../Components/Upper.component";
 import {Form, Button} from 'react-bootstrap';
 import '../App.css';
 export default class SignUp extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.onChangeSignupFirstName = this.onChangeSignupFirstName.bind(this);
+        this.onChangeSignupLastName = this.onChangeSignupLastName.bind(this);
+        this.onChangeSignupEmail = this.onChangeSignupEmail.bind(this);
+        this.onChangeSignupPassword = this.onChangeSignupPassword.bind(this);
+        this.onChangeSignupAPassword = this.onChangeSignupAPassword.bind(this);
+        this.onChangeSignupNumber = this.onChangeSignupNumber.bind(this);
+        this.onChangeSignupLocation = this.onChangeSignupLocation.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            signup_firstName: '',
+            signup_lastName: '',
+            signup_email: '',
+            signup_password: '',
+            signup_aPassword: '',
+            signup_number: '',
+            signup_location: '',
+            signup_completed: false
+        }
+    }
+
+    onChangeSignupFirstName(e){
+        this.setState({
+            signup_firstName: e.target.value
+        });
+    }
+    onChangeSignupLastName(e){
+        this.setState({
+            signup_lastName: e.target.value
+        });
+    }
+    onChangeSignupEmail(e){
+        this.setState({
+            signup_email: e.target.value
+        });
+    }
+    onChangeSignupPassword(e){
+        this.setState({
+            signup_password: e.target.value
+        });
+    }
+    onChangeSignupAPassword(e){
+        this.setState({
+            signup_aPassword: e.target.value
+        });
+    }
+    onChangeSignupNumber(e){
+        this.setState({
+            signup_number: e.target.value
+        });
+    }
+    onChangeSignupLocation(e){
+        this.setState({
+            signup_location: e.target.value
+        });
+    }
+    onSubmit(e){
+        e.preventDefault();
+        {/*on Submit logic here*/}
+        console.log(`Form submitted:`);
+        console.log(`signup First Name: ${this.state.signup_firstName}`);
+        console.log(`signup last Name: ${this.state.signup_lastName}`);
+        console.log(`signup Email: ${this.state.signup_email}`);
+        console.log(`signup Password: ${this.state.signup_password}`);
+        console.log(`signup Password Agian: ${this.state.signup_aPassword}`);
+        console.log(`signup Number: ${this.state.signup_number}`);
+        console.log(`signup Location: ${this.state.signup_location}`);
+        console.log(`signup Completed: ${this.state.signup_completed}`);
+        this.setState({
+            signup_firstName: '',
+            signup_lastName: '',
+            signup_email: '',
+            signup_password: '',
+            signup_aPassword: '',
+            signup_number: '',
+            signup_location: '',
+            signup_completed: false
+        })
+    }
+    
     render() {
         return (
             <div>
                 <Upper/>
-                <div id="login">
+                <div id="signup">
                     <div class="col-md-5 col-md-offset-5">
-                <Form>
-                    <Form.Group controlId="signFirstUpName">
-                        <Form.Label>First Name</Form.Label>
-                        <Form.Control type="name" placeholder="First Name"/>
-                    </Form.Group>
+                        <Form onSubmit={this.onSubmit}>
+                            <Form.Group controlId="firstName">
+                                <Form.Label>First Name</Form.Label>
+                                <Form.Control type="name" placeholder="firstName" value={this.state.signup_firstName} onChange={this.onChangeSignupFirstName}/>
+                            </Form.Group>
 
-                    <Form.Group controlId="signLastUpName">
-                        <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="name" placeholder="Last Name"/>
-                    </Form.Group>
+                            <Form.Group controlId="lastName">
+                                <Form.Label>Last Name</Form.Label>
+                                <Form.Control type="name" placeholder="LastName" value={this.state.signup_lastName} onChange={this.onChangeSignupLastName}/>
+                            </Form.Group>
 
-                    <Form.Group controlId="signUpPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
+                            <Form.Group controlId="password">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" value={this.state.signup_password} onChange={this.onChangeSignupPassword}/>
+                            </Form.Group>
+                        
+                            <Form.Group controlId="aPassword">
+                                <Form.Label>Confirm Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" value={this.state.signup_aPassword} onChange={this.onChangeSignupAPassword}/>
+                            </Form.Group>
 
-                    <Form.Group controlId="loginChecbox">
-                        <Form.Check type="checkbox" label="Remember me" />
-                    </Form.Group>
-                    
-                    <Button variant="primary" type="submit">
-                        Sign Up
-                    </Button>
-                </Form>
-                </div>
+                            <Form.Group controlId="Email">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Email" value={this.state.signup_email} onChange={this.onChangeSignupEmail}/>
+                            </Form.Group>
+
+                            <Form.Group controlId="Number">
+                                <Form.Label>Mobile Number</Form.Label>
+                                <Form.Control type="number" placeholder="Number" value={this.state.signup_number} onChange={this.onChangeSignupNumber}/>
+                            </Form.Group>
+
+                            <Form.Group controlId="Location">
+                                <Form.Label>Location</Form.Label>
+                                <Form.Control type="name" placeholder="Location" value={this.state.signup_location} onChange={this.onChangeSignupLocation}/>
+                            </Form.Group>
+                            
+                            <Button variant="primary" type="submit" value="Create Signup" >
+                                Sign Up
+                            </Button>
+                        </Form>
+                    </div>
                 </div>
             </div>
         )
