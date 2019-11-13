e6ffe3bf952acfc10e8bbda0742949da2cc8b604
Piyumal DK <38649090+piyumaldk@users.noreply.github.com>
2019-11-13 15:35:27 +0530

    Add Customer's add service page

---
+++ frontend/src/Pages/Customer/AddServices.component.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import LeftStaff from "../../Components/LeftCustomer.component";
+
+export default class CustomerAddServices extends Component {
+  render() {
+    return (
+      <div>
+        <LeftStaff/>
+          <div class="right">
+            Mithila methana gahapan!!  
+          </div>
+      </div>   
+    )
+  }
+}
