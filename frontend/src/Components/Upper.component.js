6431b90a4f8642bfe43abb007a10e166abd755ae
mithilajayalath <36619990+mithilajayalath@users.noreply.github.com>
2019-11-19 01:17:45 +0530

    upper component modal added

--- frontend/src/Components/Upper.component.js
+++ frontend/src/Components/Upper.component.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Nav, Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import { Nav, Navbar, Form, FormControl, Button, Modal, ButtonToolbar } from 'react-bootstrap';
 import Logo from '../Images/logo.jpg';
 
 export default class Upper extends Component {
@@ -14,6 +14,7 @@ export default class Upper extends Component {
                     <Nav className="mr-auto">
                         <Nav.Link href="/">Home</Nav.Link>
                         <Nav.Link href="/login">Log in</Nav.Link>
+                        <App />
                         <Nav.Link href="/aboutus">About us</Nav.Link>       
                     </Nav>
                     <Form inline>
@@ -23,5 +24,65 @@ export default class Upper extends Component {
                 </Navbar>
             </div>
         )
+        function MyVerticallyCenteredModal(props) {
+            return (
+              <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+              >
+                <Modal.Header closeButton>
+                  <Modal.Title id="contained-modal-title-vcenter">
+                    Sign up
+                  </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <h4>Hi Piyumal bitch</h4>
+
+                  <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
+                   
+                </Form>
+                  
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button onClick={props.onHide}>Signup</Button>
+                </Modal.Footer>
+              </Modal>
+            );
+          }
+          
+          function App() {
+            const [modalShow, setModalShow] = React.useState(false);
+          
+            return (
+              <ButtonToolbar>
+                <Nav.Link  onClick={() => setModalShow(true)}>
+                  Signup
+                </Nav.Link>
+          
+                <MyVerticallyCenteredModal
+                  show={modalShow}
+                  onHide={() => setModalShow(false)}
+                />
+              </ButtonToolbar>
+            );
+          }
+
     }
 }
