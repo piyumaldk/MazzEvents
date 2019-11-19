1846cb6aa6ea5e01b16dfd4e9eb399706a17c351
Piyumal DK <38649090+piyumaldk@users.noreply.github.com>
2019-11-19 22:57:18 +0530

    Fix authentication

--- frontend/src/Actions/authActions.js
+++ frontend/src/Actions/authActions.js
@@ -17,7 +17,7 @@ export const loadUser = () => (dispatch, getState) => {
     //User loading
     dispatch({ type: USER_LOADING});
 
-    axios.get('/mazzevents/addcustomer', tokenConfig(getState))
+    axios.get('/', tokenConfig(getState))
         .then(res => dispatch({
             type: USER_LOADED,
             payload: res.data
