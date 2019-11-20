eb4751913df3a41668f6f83b9cc3ec0fc9c2727b
kabsgithub <36620228+kabsgithub@users.noreply.github.com>
2019-11-20 10:52:38 +0530

    Update Cmail component

--- frontend/src/Pages/Customer/CmailBox.component.js
+++ frontend/src/Pages/Customer/CmailBox.component.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component,useState } from 'react';
 import LeftCustomer from "../../Components/LeftCustomer.component";
+import {Table, Nav, Form, Col, Button, Modal,Alert} from 'react-bootstrap';
 
 
 export default class CmailBox extends Component {
@@ -7,11 +8,81 @@ export default class CmailBox extends Component {
         return (
             <div>
                 <LeftCustomer/>
-                <div class="right">
-                This is LeftCustomer - Mail Box
+                <div class="mailright">
+                    <div class="mail">
+                        <center><h4>Email</h4></center>
+                        <Table striped bordered hover size="sm">
+                            
+                            <tbody>
+                                <tr>
+                                <td><Nav.Link href="">Mithila</Nav.Link> </td>
+                                </tr>
+                                <tr>
+                                <td><Nav.Link href="">Piyumal</Nav.Link></td>
+                                </tr>   
+                                <tr>
+                                <td><Nav.Link href="">Bhagaya</Nav.Link></td>
+                                </tr>
+                                <tr>
+                                <td><Nav.Link href="">Nipuni</Nav.Link></td>
+                                </tr>
+                                <tr>
+                                <td><Nav.Link href="">Yasas</Nav.Link></td>
+                                </tr>       
+                            </tbody>
+                        </Table>
+                    </div>
+
+                    <Form>
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridFrom">
+                            <Form.Control type="From" placeholder="From" />
+                            </Form.Group>
+                        </Form.Row>
+
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridTo">
+                            <Form.Control type="To" placeholder="To" />
+                            </Form.Group>
+                        </Form.Row>
+
+                        <Form.Row>
+                            <Form.Group as={Col} controlId="formGridSubject">
+                            <Form.Control type="Subject" placeholder="Subject" />
+                            </Form.Group>
+                        </Form.Row>
+            
+                        <Form.Group controlId="formGridDescription">
+                                <Form.Control as="textarea" placeholder="Compose email" rows="12" />
+                        </Form.Group>
+                    </Form>
+
+                    <Email />
                     
-                </div>
+
+                </div>  
             </div>   
         )
+        function Email() {
+            const [show, setShow] = useState(false);
+          
+            const handleClose = () => setShow(false);
+            const handleShow = () => setShow(true);
+          
+            return (
+              <>
+                <Button variant="primary" onClick={handleShow}>
+                  Send
+                </Button>
+          
+                <Modal show={show} onHide={handleClose}>
+                  
+                  <Alert variant="success">Email sent</Alert>
+                  
+                </Modal>
+              </>
+            );
+          } 
     }
 }
+
