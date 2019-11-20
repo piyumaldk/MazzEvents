9245ea7a4f36a730385a2c127a99a4848e410e19
kabsgithub <36620228+kabsgithub@users.noreply.github.com>
2019-11-20 10:52:00 +0530

    Update Cchat component

--- frontend/src/Pages/Customer/Cchat.component.js
+++ frontend/src/Pages/Customer/Cchat.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import LeftCustomer from "../../Components/LeftCustomer.component";
+import {Table, Nav, Form, Button } from 'react-bootstrap';
 
 
 export default class Cchat extends Component {
@@ -7,11 +8,77 @@ export default class Cchat extends Component {
         return (
             <div>
                 <LeftCustomer/>
-                <div class="right">
-                This is LeftCustomer - Chat
+                <div class="chatright">
+                    <div class="chat">
+                        <center><h4>Chat</h4></center>
+                        <Table striped bordered hover size="sm">
+                            
+                            <tbody>
+                                <tr>
+                                <td><Nav.Link href="">Mithila</Nav.Link> </td>
+                                </tr>
+                                <tr>
+                                <td><Nav.Link href="">Piyumal</Nav.Link></td>
+                                </tr>   
+                                <tr>
+                                <td><Nav.Link href="">Bhagaya</Nav.Link></td>
+                                </tr>
+                                <tr>
+                                <td><Nav.Link href="">Nipuni</Nav.Link></td>
+                                </tr>
+                                <tr>
+                                <td><Nav.Link href="">Yasas</Nav.Link></td>
+                                </tr>       
+                            </tbody>
+                        </Table>
+                    </div>
+                    <div class="chattop">
+
+                    </div>
                     
+                    
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        <br/>
+                        
+                        
+                        
+
+                
+                        <Form>
+                            <Form.Group controlId="formGridDescription">
+                                    
+                                    <Form.Control placeholder="Enter message here" as="textarea" rows="5" />
+                            </Form.Group>
+                        </Form>
+                        <Button variant="primary">
+                             Send
+                        </Button>
+                    
+
                 </div>
             </div>   
         )
     }
 }
+
+
