5691a1304ac70a8ac1c6804955fb549eb2a4535a
Piyumal DK <38649090+piyumaldk@users.noreply.github.com>
2019-11-30 23:46:40 +0530

    Correction

--- frontend/src/Pages/Staff/ServiceProvider.component.js
+++ frontend/src/Pages/Staff/ServiceProvider.component.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import Upper from "../../Components/Upper.component";
-import {Form, Button, Col} from 'react-bootstrap';
+import {Form, Button} from 'react-bootstrap';
 import '../../App.css';
 import axios from 'axios';
-
+import LeftStaff from "../../Components/LeftStaff.component";
+import {Alert} from 'reactstrap';
 export default class StaffServiceProvider extends Component {
 
     constructor(props){
@@ -11,7 +11,6 @@ export default class StaffServiceProvider extends Component {
 
         this.onChangeSignupFirstName = this.onChangeSignupFirstName.bind(this);
         this.onChangeSignupLastName = this.onChangeSignupLastName.bind(this);
-        this.onChangeSignupOption = this.onChangeSignupOption.bind(this);
         this.onChangeSignupEmail = this.onChangeSignupEmail.bind(this);
         this.onChangeSignupPassword = this.onChangeSignupPassword.bind(this);
         this.onChangeSignupAPassword = this.onChangeSignupAPassword.bind(this);
@@ -22,7 +21,6 @@ export default class StaffServiceProvider extends Component {
         this.state = {
             signup_firstName: '',
             signup_lastName: '',
-            signup_option: '',
             signup_email: '',
             signup_password: '',
             signup_aPassword: '',
@@ -40,11 +38,6 @@ export default class StaffServiceProvider extends Component {
     onChangeSignupLastName(e){
         this.setState({
             signup_lastName: e.target.value
-        });
-    }
-    onChangeSignupOption(e){
-        this.setState({
-            signup_option: e.target.value
         });
     }
     onChangeSignupEmail(e){
@@ -77,7 +70,6 @@ export default class StaffServiceProvider extends Component {
         console.log(`Form submitted:`);
         console.log(`signup First Name: ${this.state.signup_firstName}`);
         console.log(`signup last Name: ${this.state.signup_lastName}`);
-        console.log(`signup option: ${this.state.signup_option}`);
         console.log(`signup Email: ${this.state.signup_email}`);
         console.log(`signup Password: ${this.state.signup_password}`);
         console.log(`signup Password Agian: ${this.state.signup_aPassword}`);
@@ -94,25 +86,16 @@ export default class StaffServiceProvider extends Component {
             signup_aPassword: this.state.signup_aPassword,
             signup_number: this.state.signup_number,
             signup_location: this.state.signup_location,
-            signup_completed: this.state.signup_completed
+            signup_completed: this.state.signup_completed,
+            msg: this.props.msg
         }
 
-        if(this.state.signup_option==="1"){
-            axios.post('http://localhost:4000/mazzevents/addserviceprovider', newSignUp)
-                .then(res => console.log(res.data));
-        }
-        else{
-            axios.post('http://localhost:4000/mazzevents/addcustomer', newSignUp)
-                .then(res => console.log(res.data));
-        }
-
-        // axios.post('http://localhost:4001/mazzevent/customers', newSignUp)
-        //         .then(res => console.log(res.data));
+        axios.post('http://localhost:4000/mazzevents/addserviceprovider', newSignUp)
+            .then(res => console.log(res.data));
         
         this.setState({
             signup_firstName: '',
             signup_lastName: '',
-            singup_option: '',
             signup_email: '',
             signup_password: '',
             signup_aPassword: '',
@@ -120,13 +103,14 @@ export default class StaffServiceProvider extends Component {
             signup_location: '',
             signup_completed: false
         })
+        
     }
     
     render() {
         return (
             <div>
-                <Upper/>
-                <div id="signup">
+                <LeftStaff/>
+                <div id="signup" class="right">
                     <div class="col-md-5 col-md-offset-5">
                         <Form onSubmit={this.onSubmit}>
                             <Form.Group controlId="firstName">
@@ -137,14 +121,6 @@ export default class StaffServiceProvider extends Component {
                             <Form.Group controlId="lastName">
                                 <Form.Label>Last Name</Form.Label>
                                 <Form.Control type="name" placeholder="LastName" value={this.state.signup_lastName} onChange={this.onChangeSignupLastName}/>
-                            </Form.Group>
-
-                            <Form.Group as={Col} controlId="formGridState">
-                                <Form.Label>Are you a Free User or a Service Provider?</Form.Label>
-                                <select name="cars" type="name" value={this.state.signup_option} onChange={this.onChangeSignupOption}>
-                                    <option value="0">Free User</option>
-                                    <option value="1">Service Provider</option>
-                                </select>
                             </Form.Group>
 
                             <Form.Group controlId="password">
@@ -172,8 +148,10 @@ export default class StaffServiceProvider extends Component {
                                 <Form.Control type="name" placeholder="Location" value={this.state.signup_location} onChange={this.onChangeSignupLocation}/>
                             </Form.Group>
 
+                            { this.state.msg ? (<Alert color="danger">{ this.state.msg }</Alert>) : null }
+
                             <Button variant="primary" type="submit" value="Create Signup" >
-                                Sign Up
+                                Add Service Provider
                             </Button>
                         </Form>
                     </div>
@@ -182,3 +160,11 @@ export default class StaffServiceProvider extends Component {
         )
     }
 }
+
+const mapStateToProps = state => ({
+    isAuthenticated: PropTypes.bool,
+    error: PropTypes.object.isRequired,
+    register: PropTypes.func.isRequired,
+    clearErrors: PropTypes.func.isRequired
+  });
+  
