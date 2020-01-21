cfd303f8b7399025319554920eb9dfdc78ed8080
Piyumal DK <38649090+piyumaldk@users.noreply.github.com>
2020-01-21 18:53:11 +0530

    Lower case searching

--- frontend/src/Pages/Customer/Photographers.component.js
+++ frontend/src/Pages/Customer/Photographers.component.js
@@ -39,9 +39,6 @@ export default class Photgraphers extends Component {
     constructor(props) {
         super(props);
         this.state = {users: []};
-        //this.onChangeLocation = this.onChangeLocation.bind(this);
-
-        // this.state = {location: ''}
     }
 
     componentDidMount() {
@@ -56,16 +53,16 @@ export default class Photgraphers extends Component {
     }
 
     onChange = e => {
-        this.setState({ location: e.target.value});
+        this.setState({ location: e.target.value.toLowerCase()});
         
     };
    
-    
 
-    UserList (state) {
+
+    UserList () {
         const local = this.state.location;
         //local = local.toUpperCase();
-
+        //const m = local.toUpperCase();
         console.log(local);
         //local = local.filterText.toLowerCase();
         if(local ==null || local==""){
@@ -78,7 +75,7 @@ export default class Photgraphers extends Component {
         }
         else{
             return this.state.users.map(function(currentSignUpCustomer, i){
-                if(currentSignUpCustomer.signup_type === "2" && currentSignUpCustomer.signup_category === "Photographer" && currentSignUpCustomer.signup_city === local ){
+                if(currentSignUpCustomer.signup_type === "2" && currentSignUpCustomer.signup_category === "Photographer" && currentSignUpCustomer.signup_city.toLowerCase() === local ){
                 return <SignUpCustomer signupcustomer={currentSignUpCustomer} key={i} />;
                 }
                 return null;
@@ -117,92 +114,3 @@ export default class Photgraphers extends Component {
         )
     }
 }
-// import React, { Component } from 'react';
-// import LeftCustomer from "../../Components/LeftCustomer.component";
-
-// import {Form, Button,Card, CardDeck} from 'react-bootstrap';
-// import '../../App.css';
-// import axios from 'axios';
-
-// import {Alert} from 'reactstrap';
-// import Upper from "../../Components/Upper.component";
-// import { Link } from 'react-router-dom';
-// import SignUpCatering from '../../Components/Auth/RegisterCateringModal';
-// // import catering from '../../Images/catering.jpg';
-// import img1 from '../../Images/Customer/dj.jpg';
-// const type = props =>props.signupcustomer.signup_type;
-
-// const SignUpCustomer = props => (
-//     <div>
-        
-            
-//         <Card  bg="light" text="black" style={{ width: '18rem'  }}>
-            
-//         <Card.Img variant="top" src={img1} />
-//         <Card.Header><center>Are you from around {props.signupcustomer.signup_city}?</center></Card.Header>
-//             <Card.Body>
-//             <Card.Title><center>{props.signupcustomer.signup_company}</center></Card.Title>
-//             <Card.Text>
-//                 Owner : {props.signupcustomer.signup_firstName} {props.signupcustomer.signup_lastName}<br/>
-//                 Contact Number : {props.signupcustomer.signup_number}<br/>
-//                 Location : {props.signupcustomer.signup_city}<br/>
-//                 Address : {props.signupcustomer.signup_address2}<br/>
-//             </Card.Text>
-//             <center><Button variant="dark">Go somewhere</Button></center>
-//             </Card.Body>
-//         </Card>  
-        
-         
-//     </div>
-// )
-
-// export default class Photgraphers extends Component {
-
-//     constructor(props) {
-//         super(props);
-//         this.state = {users: []};
-//     }
-
-//     componentDidMount() {
-//         axios.get('http://localhost:4000/mazzevents/')
-//             .then(response => {
-//                 this.setState({ users: response.data });
-//             })
-//             .catch(function (error){
-//                 console.log(error);
-//             })
-//     }
-
-//     UserList() {
-//         return this.state.users.map(function(currentSignUpCustomer, i){
-//             if(currentSignUpCustomer.signup_type == "2" && currentSignUpCustomer.signup_category == "Photgrapher"){
-//             return <SignUpCustomer signupcustomer={currentSignUpCustomer} key={i} />;
-//             }
-//         })
-//     }
-
-//     render() {
-
-        
-//         return (
-//             <div>
-//                 <LeftCustomer/>
-//                 <div class="right">
-//                     <Upper/>
-//                     <div>
-                        
-                        
-//                           <div  className="row card_ss">
-//                           <CardDeck>
-//                             { this.UserList() } 
-//                             </CardDeck>
-//                             </div>   
-                        
-                        
-                         
-//                     </div>
-//                 </div>
-//             </div>   
-//         )
-//     }
-// }
