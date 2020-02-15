baf2d8c08275b7b779913b11437304ba0046464c
kabsgithub <36620228+kabsgithub@users.noreply.github.com>
2020-02-15 12:26:44 +0530

    Update Email component

--- frontend/src/Pages/ServiceProvider/MailBox.component.js
+++ frontend/src/Pages/ServiceProvider/MailBox.component.js
@@ -1,87 +1,90 @@
-import React, { Component, useState } from 'react';
-import LeftSeriveProvider from "../../Components/LeftServiceProvider.component";
-import {Table, Nav, Form, Col, Button, Modal,Alert} from 'react-bootstrap';
-import Upper from "../../Components/Upper.component";
-export default class ServiceProviderMailBox extends Component {
-    render() {
+
+    import React, { Component } from 'react';
+    import { connect } from 'react-redux';
+    import Upper from "../../Components/Upper.component";
+    import { Table, Nav, Form, Col, Button, Modal, Alert } from 'react-bootstrap';
+    import LeftSeriveProvider from "../../Components/LeftServiceProvider.component";
+    
+    
+    class ServiceProviderMailBox extends Component {
+    
+    
+    
+      state = {
+        email: {
+          recipient: '',
+          sender: this.props.email,
+          subject: '',
+          text: '',
+        }
+      }
+    
+    
+      sendEmail = _ => {
+        const { email } = this.state;
+        fetch(`http://127.0.0.1:4000/send-email?recipient=${email.recipient}&sender=${email.sender}&topic=${email.subject}&text=${email.text}`) //query string url
+          .catch(err => console.error(err))
+    
+        this.props.history.push('/staff/mailbox');
+      }
+    
+      render() {
+        const { email } = this.state;
+        const spacer = {
+          margin: 10
+        }
+        const textArea = {
+          borderRadius: 8
+        }
         return (
-            <div>
-                <LeftSeriveProvider/>
-                <div className="mailright">
-                <Upper/>
-                    <div className="mail">
-                        <center><h4>Email</h4></center>
-                        <Table striped bordered hover size="sm">
-                            
-                            <tbody>
-                                <tr>
-                                <td><Nav.Link href="">Mithila</Nav.Link> </td>
-                                </tr>
-                                <tr>
-                                <td><Nav.Link href="">Piyumal</Nav.Link></td>
-                                </tr>   
-                                <tr>
-                                <td><Nav.Link href="">Bhagaya</Nav.Link></td>
-                                </tr>
-                                <tr>
-                                <td><Nav.Link href="">Nipuni</Nav.Link></td>
-                                </tr>
-                                <tr>
-                                <td><Nav.Link href="">Yasas</Nav.Link></td>
-                                </tr>       
-                            </tbody>
-                        </Table>
-                    </div>
-
-                    <Form>
-                        <Form.Row>
-                            <Form.Group as={Col} controlId="formGridFrom">
-                            <Form.Control type="From" placeholder="From" />
-                            </Form.Group>
-                        </Form.Row>
-
-                        <Form.Row>
-                            <Form.Group as={Col} controlId="formGridTo">
-                            <Form.Control type="To" placeholder="To" />
-                            </Form.Group>
-                        </Form.Row>
-
-                        <Form.Row>
-                            <Form.Group as={Col} controlId="formGridSubject">
-                            <Form.Control type="Subject" placeholder="Subject" />
-                            </Form.Group>
-                        </Form.Row>
-            
-                        <Form.Group controlId="formGridDescription">
-                                <Form.Control as="textarea" placeholder="Compose email" rows="12" />
-                        </Form.Group>
-                    </Form>
-
-                    <Email />
-                    
-
-                </div>  
-            </div>   
+    
+          <div className="row">
+            <div className="col-md-2">
+              <LeftSeriveProvider />
+            </div>
+            <div className="col-md-10">
+              <div >
+                <Upper />
+    
+                <div className="App">
+                  <div style={{ marginTop: 10 }} >
+                    <h2> Send Email </h2>
+                    <form>
+                      <label> Recipient </label>
+                      <br />
+                      <input value={email.recipient} style={{ width: 800 }} className="bodr"
+                        onChange={e => this.setState({ email: { ...email, recipient: e.target.value } })} />
+                      <div style={spacer} />
+                      <label> Subject </label>
+                      <br />
+                      <input value={email.subject} style={{ width: 800 }} className="bodr"
+                        onChange={e => this.setState({ email: { ...email, subject: e.target.value } })} />
+                      <div style={spacer} />
+                      <label> Message </label>
+                      <br />
+                      <textarea rows={3} value={email.text} style={textArea} style={{ width: 800 }}
+                        onChange={e => this.setState({ email: { ...email, text: e.target.value } })} />
+                    </form>
+                    <div style={spacer} />
+                    <Button onClick={this.sendEmail}> Send Email </Button>
+                  </div>
+                </div>
+    
+              </div>
+            </div>
+    
+          </div>
         )
-        function Email() {
-            const [show, setShow] = useState(false);
-          
-            const handleClose = () => setShow(false);
-            const handleShow = () => setShow(true);
-          
-            return (
-              <>
-                <Button variant="primary" onClick={handleShow}>
-                  Send
-                </Button>
-          
-                <Modal show={show} onHide={handleClose}>
-                  
-                  <Alert variant="success">Email sent</Alert>
-                  
-                </Modal>
-              </>
-            );
-          }
+      }
     }
-}
+    
+    const mapStateToProps = state => ({
+      id: state.auth.id,
+      fName: state.auth.fName,
+      lName: state.auth.lName,
+      email: state.auth.email,
+      number: state.auth.number
+    });
+    
+    export default connect(mapStateToProps, null)(ServiceProviderMailBox);
+    
