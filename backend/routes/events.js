addc6c58c5ebc1425521d37ae88d82bd7b7c4eb1
Piyumal DK <38649090+piyumaldk@users.noreply.github.com>
2020-02-17 19:31:28 +0530

    EDU

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -15,8 +15,24 @@ router.route('/').get(function(req, res) {
             res.json(event);
         }
     }).sort({$natural:-1}).limit(8);
-  });
+});
 
+router.route('/dfdf').get(function(req, res) {
+    Events.find(function(err, event) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.json(event);
+        }
+    }).sort({$natural:-1});
+});
+
+router.route('/:id').get(function (req, res) {
+    let id = req.params.id;
+    Events.findById(id, function (err, events) {
+      res.json(events);
+    });
+});
 router.post('/addevent', (req, res) => {
     const {eventName, location, time, link} = req.body;
     //Simple Validation (Emty Form)
@@ -34,4 +50,30 @@ router.post('/addevent', (req, res) => {
       
   });
 
-  module.exports = router;
+router.route('/updateevent/:id').post(function (req, res) {
+    Events.findById(req.params.id, function (err, event) {
+      if (!event) {
+        res.status(404).send("data is not found");
+      }
+      else
+        event.eventName = req.body.eventName;
+        event.location = req.body.location;
+        event.time = req.body.time;
+        event.link = req.body.link;
+        event.event_completed = req.body.event_completed;
+        event.save().then(event => {
+        })
+        .catch(err => {
+          res.status(400).send("Update not possible");
+        });
+    });
+  });
+
+router.delete('/removeevent/:id', function (req, res) { 
+    Events.findByIdAndRemove({ _id: req.params.id })
+    .then(function (docs) {
+      res.send({message: "User successfully deleted!", state: true});
+    }); 
+});
+
+module.exports = router;
---
+++ frontend/src/Pages/Staff/EditEvent.component.js
@@ -0,0 +1,169 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import LeftStaff from "../../Components/LeftStaff.component";
+import Upper from "../../Components/Upper.component";
+import { Button, Card, Form, Col } from 'react-bootstrap';
+
+ class EditEvent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.deleteUser = this.deleteUser.bind(this);
+
+        this.onChangeEventName = this.onChangeEventName.bind(this);
+        this.onChangeLocation = this.onChangeLocation.bind(this);
+        this.onChangeTime = this.onChangeTime.bind(this);
+        this.onChangeLink = this.onChangeLink.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+
+        this.state = {
+            eventName: '',
+            location: '',
+            time: '',
+            link: '',
+            event_completed: false,
+            _id: ''
+        }
+    }
+    
+    componentDidMount() {
+        axios.get('http://localhost:4000/events/'+this.props.match.params.id)
+            
+            .then(response => {
+                this.setState({
+                    //profile_data: response.data,
+                    eventName: response.data.eventName,
+                    location: response.data.location,
+                    time: response.data.time,
+                    link: response.data.link,
+                    event_completed: response.data.event_completed,
+                    _id: response.data._id
+                })
+            })
+            .catch(function (error) {
+                console.log(error)
+            })
+    }
+
+    onChangeEventName(e) {
+        this.setState({
+            eventName: e.target.value
+        });
+    }
+    onChangeLocation(e) {
+        this.setState({
+            location: e.target.value
+        });
+    }
+    onChangeTime(e) {
+        this.setState({
+            time: e.target.value
+        });
+    }
+    onChangeLink(e) {
+        this.setState({
+            link: e.target.value
+        });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+
+        const obj = {
+            _id: this.state._id,
+            eventName: this.state.eventName,
+            location: this.state.location,
+            time: this.state.time,
+            link: this.state.link,
+            event_completed: this.state.event_completed
+        };
+        axios.post('http://localhost:4000/events/updateevent/' + this.props.match.params.id, obj)
+            .then(res => console.log(res.data));
+
+        this.props.history.push('/staff/eventlist');
+        window.location.reload();
+        alert("Updated successfully")
+    }
+
+    deleteUser(e) {
+        e.preventDefault();
+        alert("Are you sure?");
+        var id = this.props.match.params.id;
+        //console.log(id);
+        axios.delete('http://localhost:4000/events/removeevent/' + id, {})
+            .then(res => console.log(res));
+
+        this.props.history.push('/staff/eventlist');
+        window.location.reload();
+    }
+
+    render() {
+        console.log(this.props.match.params.id);
+        
+        return (
+            <div>
+                <LeftStaff />
+                <div class="right">
+                    <Upper />
+                    <div className="frm">
+                        <div className="txt">
+                            <h3 >Edit the event</h3>
+                        </div>
+
+                        <form onSubmit={this.onSubmit} >
+
+                            <div className="form-group">
+                                <label>Event Name</label>
+                                <input type="text"
+                                    className="form-control"
+                                    value={this.state.eventName}
+                                    onChange={this.onChangeEventName}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Event Location</label>
+                                <input type="text"
+                                    className="form-control"
+                                    value={this.state.location}
+                                    onChange={this.onChangeLocation}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Time</label>
+                                <input type="text"
+                                    className="form-control"
+                                    value={this.state.time}
+                                    onChange={this.onChangeTime}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Link</label>
+                                <input type="text"
+                                    className="form-control"
+                                    value={this.state.link}
+                                    onChange={this.onChangeLink}
+                                />
+                            </div>
+                            
+                            <div className="form-group">
+                                <br />
+                                <input type="submit" value="Update the Event" className="btn btn-primary" />
+                            </div>
+                            
+
+                        </form>
+                            <div>
+                                <Button onClick={this.deleteUser}>Delete</Button>
+                            </div>
+
+                    </div>
+
+                </div>
+
+
+            </div>
+
+        )
+    }
+}
+export default (EditEvent);
