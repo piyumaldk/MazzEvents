c5ba7064aee5dd819f389a17c1221d8423773951
kabsgithub <36620228+kabsgithub@users.noreply.github.com>
2020-02-17 22:41:12 +0530

    Update More component

--- frontend/src/Pages/Customer/More.component.js
+++ frontend/src/Pages/Customer/More.component.js
@@ -208,7 +208,7 @@ class More extends Component {
                         <div className="row card_ss">
                             <div className="col-md-4">
 
-                                <Card style={{ width: '18rem', height: '27rem' }}>
+                                <Card style={{ width: '27rem', height: '27rem' }}>
                                     <div className="overflow">
                                         <Card.Img variant="top" height="240" src={!this.state.businessPic ? company : this.state.businessPic} />
                                     </div>
